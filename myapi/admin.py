97839f470a5afa199142f7234cc187ab133b3d96
Mahdi Hamldar <mahdihamldar@gmail.com>
2021-12-26 15:48:56 +0330

    Team model admin panel customization

--- myapi/admin.py
+++ myapi/admin.py
@@ -3,4 +3,12 @@ from .models import Team
 
 # Register your models here.
 
-admin.site.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('TeamId', 'OrganizationId', 'BusinessUnitId', 'Name', 'Description', 'CreatedOn', 'ModifiedOn', 'CreatedBy',
+    'ModifiedBy', 'IsDefault', 'AdministratorId', 'QueueId', 'TeamType', 'TeamTemplateId', 'RegardingObjectTypeCode', 'Manager',
+    'SalesPurchaseManager', 'Id', 'WegeDetailAccount', 'PoursantDetailAccount', 'Subsidiary', 'WegeFactorsDetailAccount',
+    'PoursantFactorsDetailAccount', 'UnitManager', 'TeamPercentage', 'Goal', 'UserCount', 'NewTeamType')
+    list_filter = ("CreatedBy",)
+    search_fields = ['Name', 'TeamId']
+
+admin.site.register(Team, TeamAdmin)
