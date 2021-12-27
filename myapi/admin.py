dabbd70e5abb5612b8cb380f4a3deda81a3840d5
Mahdi Hamldar <mahdihamldar@gmail.com>
2021-12-27 16:21:19 +0330

    Systemusersview Model added to admin panel

--- myapi/admin.py
+++ myapi/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from .models import Team
+from .models import Systemusersview
 
 # Register your models here.
 
+### Team Model ###
 class TeamAdmin(admin.ModelAdmin):
     list_filter = ("teamid", "createdby")
     search_fields = ['Name', 'TeamId']
 
 admin.site.register(Team, TeamAdmin)
+
+
+### Systemusersview Model ###
+class SystemusersviewAdmin(admin.ModelAdmin):
+    list_filter = ("mainteam", "createdby")
+    search_fields = ['fullname', 'mobilephone']
+
+admin.site.register(Systemusersview, SystemusersviewAdmin)
